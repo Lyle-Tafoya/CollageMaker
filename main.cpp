35fe657a52876f066da86c81fb6f5e8c13ddd8bb
Lyle tafoya <lyle.tafoya@gmail.com>
2018-06-11 16:11:47 -0700

    Fixed an ambiguous call to Image

--- main.cpp
+++ main.cpp
@@ -6,8 +6,6 @@
 #include <cstring>
 #include <ctime>
 #include <Magick++.h>
-using namespace Magick;
-using namespace std;
 
 // Generate a random number between from and to
 size_t RandNum(size_t from, size_t to) { return (rand() % (to-from+1)) + from; }
@@ -15,15 +13,15 @@ size_t RandNum(size_t from, size_t to) { return (rand() % (to-from+1)) + from; }
 // Information needed in order to queue a image draw operation
 struct DrawOperation
 {
-  Image *pImage;
+  Magick::Image *pImage;
   size_t x;
   size_t y;
-  DrawOperation(Image *image, size_t startX, size_t startY)
+  DrawOperation(Magick::Image *image, size_t startX, size_t startY)
     : pImage(image), x(startX), y(startY) { drawQueue.push_back(this); };
   ~DrawOperation() { delete pImage; }
 
-  static vector<DrawOperation *> drawQueue;
-  static void Draw(Image &canvas)
+  static std::vector<DrawOperation *> drawQueue;
+  static void Draw(Magick::Image &canvas)
   {
     for(int i = 0; i < drawQueue.size(); i++)
     {
@@ -32,25 +30,25 @@ struct DrawOperation
     }
   }
 };
-vector<DrawOperation *> DrawOperation::drawQueue;
+std::vector<DrawOperation *> DrawOperation::drawQueue;
 
 // Read a text file into memory
 struct textfile
 {
 private:
-  vector<string> lines;
+  std::vector<std::string> lines;
 
 public:
-  textfile(string fileName)
+  textfile(std::string fileName)
   {
-    ifstream pFile(fileName);
-    string line;
+    std::ifstream pFile(fileName);
+    std::string line;
     while(getline(pFile, line))
       lines.push_back(line);
     pFile.close();
   }
 
-  string GetLine(size_t lineNum) const { return lines[lineNum]; }
+  std::string GetLine(size_t lineNum) const { return lines[lineNum]; }
   void DelLine(size_t lineNum) { lines.erase(lines.begin()+lineNum); }
   size_t NumLines() const { return lines.size(); }
 };
@@ -62,7 +60,7 @@ public:
 // Default resolution of output image
 size_t screenWidth = 1920, screenHeight = 1080;
 // Default background color
-string canvasColor = "black";
+std::string canvasColor = "black";
 // Default size of main image as a percentage
 unsigned char size = 65;
 // Corner to place the first image
@@ -71,9 +69,9 @@ unsigned char corner;
 size_t minDraw = 150;
 bool tileType;
 // File extension
-string filename = "collage.png";
+std::string filename = "collage.png";
 // Path to image database
-string database = "images.txt";
+std::string database = "images.txt";
 
 textfile *pImages;
 
@@ -84,7 +82,7 @@ textfile *pImages;
 void TileImages(size_t width, size_t height, size_t xOrigin, size_t yOrigin, size_t minDraw, bool tileType)
 {
   size_t x = xOrigin, y = yOrigin;
-  vector<DrawOperation *> drawQueue;
+  std::vector<DrawOperation *> drawQueue;
   size_t imageNum;
 
   // Tile Horizontally
@@ -93,9 +91,9 @@ void TileImages(size_t width, size_t height, size_t xOrigin, size_t yOrigin, siz
     while(1)
     {
       imageNum = RandNum(0, pImages->NumLines()-1);
-      Image *pImage = new Image(pImages->GetLine(imageNum));
+      Magick::Image *pImage = new Magick::Image(pImages->GetLine(imageNum));
       pImages->DelLine(imageNum);
-      pImage->resize(Geometry("x" + to_string(height)));
+      pImage->resize(Magick::Geometry("x" + std::to_string(height)));
       // If the last image wont fit, tile images in the empty space
       if(((x-xOrigin) + pImage->columns()) > width)
       {
@@ -122,9 +120,9 @@ void TileImages(size_t width, size_t height, size_t xOrigin, size_t yOrigin, siz
     while(1)
     {
       imageNum = RandNum(0, pImages->NumLines()-1);
-      Image *pImage = new Image(pImages->GetLine(imageNum));
+      Magick::Image *pImage = new Magick::Image(pImages->GetLine(imageNum));
       pImages->DelLine(imageNum);
-      pImage->resize(Geometry(to_string(width)));
+      pImage->resize(Magick::Geometry(std::to_string(width)));
       // If the last image wont fit, tile images in the empty space
       if((y-yOrigin + pImage->rows()) > height)
       {
@@ -158,27 +156,27 @@ int main(int argc, char *argv[])
   for(int i = 1; i < argc; i++)
   {
     if(strcmp(argv[i], "--width") == 0 || strcmp(argv[i], "-w") == 0)
-      screenWidth = stoi(argv[++i]);
+      screenWidth = std::stoi(argv[++i]);
     else if(strcmp(argv[i], "--height") == 0 || strcmp(argv[i], "-h") == 0)
-      screenHeight = stoi(argv[++i]);
+      screenHeight = std::stoi(argv[++i]);
     else if(strcmp(argv[i], "--color") == 0 || strcmp(argv[i], "-c") == 0)
       canvasColor = argv[++i];
     else if(strcmp(argv[i], "--minDraw") == 0 || strcmp(argv[i], "-d") == 0)
-      minDraw = stoi(argv[++i]);
+      minDraw = std::stoi(argv[++i]);
     else if(strcmp(argv[i], "--file") == 0 || strcmp(argv[i], "-f") == 0)
       filename = argv[++i];
     else if(strcmp(argv[i], "--size") == 0 || strcmp(argv[i], "-s") == 0)
-      size = stoi(argv[++i]);
+      size = std::stoi(argv[++i]);
     else if(strcmp(argv[i], "--type") == 0 || strcmp(argv[i], "-t") == 0)
     {
-      string tiletype = argv[++i];
+      std::string tiletype = argv[++i];
       if(tiletype == "vertical" || tiletype == "v")
         tileType = 1;
       else // Horizontal
         tileType = 0;
     }
     else if(strcmp(argv[i], "--corner") == 0 || strcmp(argv[i], "-r") == 0)
-      corner = stoi(argv[++i]);
+      corner = std::stoi(argv[++i]);
     else if(strcmp(argv[i], "--path") == 0 || strcmp(argv[i], "-p") == 0)
       database = argv[++i];
   }
@@ -186,10 +184,11 @@ int main(int argc, char *argv[])
   pImages = new textfile(database);
 
   // Set up our blank canvas
-  Image canvas(to_string(screenWidth)+"x"+to_string(screenHeight), canvasColor.c_str());
+  Magick::Geometry geometry(std::to_string(screenWidth)+"x"+std::to_string(screenHeight));
+  Magick::Image canvas(geometry, canvasColor.c_str());
 
   size_t imageNum = RandNum(0, pImages->NumLines()-1);
-  Image mainImage(pImages->GetLine(imageNum));
+  Magick::Image mainImage(pImages->GetLine(imageNum));
   pImages->DelLine(imageNum);
 
   // Downscale the image to the specified size but never upscale
@@ -197,13 +196,13 @@ int main(int argc, char *argv[])
   {
     size_t imageWidth = (float)size / 100.f * (float)screenWidth;
     if(mainImage.columns() > imageWidth)
-      mainImage.resize(Geometry(to_string(imageWidth)));
+      mainImage.resize(Magick::Geometry(std::to_string(imageWidth)));
   }
   else
   {
     size_t imageHeight = (float)size / 100.f * (float)screenHeight;
     if(mainImage.rows() > imageHeight)
-      mainImage.resize(Geometry("x" + to_string(imageHeight)));
+      mainImage.resize(Magick::Geometry("x" + std::to_string(imageHeight)));
   }
 
   // Randomly pick a corner to draw the first image
@@ -235,7 +234,7 @@ int main(int argc, char *argv[])
 
   // Draw the first image
   DrawOperation *pDrawOp = new DrawOperation(&mainImage, mainX, mainY);
-  
+
   // Tile images horizontally first and then vertically
   if(tileType == 0)
   {
