60c7bb6f878ee2f1b125e5c6192ec1643fc592b0
Lyle-Tafoya <lyle.tafoya@gmail.com>
2018-11-24 18:08:37 -0800

    Added gflags support

--- src/main.cpp
+++ src/main.cpp
@@ -5,79 +5,37 @@
 #include <memory>
 #include <string>
 #include <vector>
+
+#include <gflags/gflags.h>
 #include <Magick++.h>
 
 #include "tiler.hpp"
 
-// Default resolution of output image
-size_t outputWidth = 1920, outputHeight = 1080;
-// Default background color
-std::string canvasColor = "black";
-// Default size of main image as a percentage
-unsigned char size = 65;
-// Corner to place the first image
-unsigned char corner;
-// Do not attempt to draw in areas less than minDraw number of pixels
-size_t minDraw = 150;
-bool tileType;
-// File extension
-std::string filename = "collage.png";
-// Path to image database
-std::string database = "images.txt";
+DEFINE_int32(outputWidth, 1920, "Width of the output image");
+DEFINE_int32(outputHeight, 1080, "Height of the output image");
+DEFINE_string(canvasColor, "black", "Color of the canvas background");
+DEFINE_string(outputFilename, "collage.png", "Ouput filename for the collage image");
+DEFINE_string(inputFilenames, "images.txt", "image filepath list");
+DEFINE_int32(minDraw, 150, "Minimum amount of pixels to tile images in");
+DEFINE_int32(corner, 0, "Specify which corner to place first image");
+DEFINE_int32(size, 65, "Size of the first image as a percentage relative to the canvas");
+DEFINE_string(tileType, "vertical", "Set which orthogonal direction to begin tiling");
 
 int main(int argc, char *argv[])
 {
   srand(time(NULL));
 
-  corner = CollageMaker::randNum(0, 3);
-  tileType = CollageMaker::randNum(0, 1);
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  bool tileType = (FLAGS_tileType == "vertical" || FLAGS_tileType == "v") ? 1 : 0;
 
-  // Handle our command line arguments
-  for(int i = 1; i < argc; i++)
-  {
-    if(strcmp(argv[i], "--width") == 0 || strcmp(argv[i], "-w") == 0)
-      outputWidth = std::stoi(argv[++i]);
-    else if(strcmp(argv[i], "--height") == 0 || strcmp(argv[i], "-h") == 0)
-      outputHeight = std::stoi(argv[++i]);
-    else if(strcmp(argv[i], "--color") == 0 || strcmp(argv[i], "-c") == 0)
-      canvasColor = argv[++i];
-    else if(strcmp(argv[i], "--minDraw") == 0 || strcmp(argv[i], "-d") == 0)
-      minDraw = std::stoi(argv[++i]);
-    else if(strcmp(argv[i], "--file") == 0 || strcmp(argv[i], "-f") == 0)
-      filename = argv[++i];
-    else if(strcmp(argv[i], "--size") == 0 || strcmp(argv[i], "-s") == 0)
-      size = std::stoi(argv[++i]);
-    else if(strcmp(argv[i], "--type") == 0 || strcmp(argv[i], "-t") == 0)
-    {
-      std::string tiletype = argv[++i];
-      if(tiletype == "vertical" || tiletype == "v")
-      {
-        tileType = 1;
-      }
-      else // Horizontal
-      {
-        tileType = 0;
-      }
-    }
-    else if(strcmp(argv[i], "--corner") == 0 || strcmp(argv[i], "-r") == 0)
-    {
-      corner = std::stoi(argv[++i]);
-    }
-    else if(strcmp(argv[i], "--path") == 0 || strcmp(argv[i], "-p") == 0)
-    {
-      database = argv[++i];
-    }
-  }
-
-  CollageMaker::Tiler tiler(outputWidth, outputHeight, database);
+  CollageMaker::Tiler tiler(FLAGS_outputWidth, FLAGS_outputHeight, FLAGS_inputFilenames, FLAGS_canvasColor);
 
   size_t imageNum = CollageMaker::randNum(0, tiler.imageFilePaths.size()-1);
   Magick::Image mainImage(tiler.popImagePath(imageNum));
-
   // Downscale the image to the specified size but never upscale
-  if((float)outputWidth/(float)mainImage.columns() < (float)outputHeight/(float)mainImage.rows())
+  if((float)FLAGS_outputWidth/(float)mainImage.columns() < (float)FLAGS_outputHeight/(float)mainImage.rows())
   {
-    size_t imageWidth = (float)size / 100.f * (float)outputWidth;
+    size_t imageWidth = (float)FLAGS_size / 100.f * (float)FLAGS_outputWidth;
     if(mainImage.columns() > imageWidth)
     {
       mainImage.resize(Magick::Geometry(std::to_string(imageWidth)));
@@ -85,7 +43,7 @@ int main(int argc, char *argv[])
   }
   else
   {
-    size_t imageHeight = (float)size / 100.f * (float)outputHeight;
+    size_t imageHeight = (float)FLAGS_size / 100.f * (float)FLAGS_outputHeight;
     if(mainImage.rows() > imageHeight)
     {
       mainImage.resize(Magick::Geometry("x" + std::to_string(imageHeight)));
@@ -94,9 +52,8 @@ int main(int argc, char *argv[])
   const size_t mainImageWidth = mainImage.columns();
   const size_t mainImageHeight = mainImage.rows();
 
-  // Randomly pick a corner to draw the first image
   size_t mainX = 0, mainY = 0;
-  switch(corner)
+  switch(FLAGS_corner)
   {
     // Top Left
     case 0:
@@ -105,36 +62,37 @@ int main(int argc, char *argv[])
       break;
     // Top Right
     case 1:
-      mainX = outputWidth - mainImageWidth;
+      mainX = FLAGS_outputWidth - mainImageWidth;
       mainY = 0;
       break;
     // Bottom Right
     case 2:
-      mainX = outputWidth - mainImageWidth;
-      mainY = outputHeight - mainImageHeight;
+      mainX = FLAGS_outputWidth - mainImageWidth;
+      mainY = FLAGS_outputHeight - mainImageHeight;
       break;
     // Bottom Left
     case 3:
       mainX = 0;
-      mainY = outputHeight - mainImageHeight;
+      mainY = FLAGS_outputHeight - mainImageHeight;
       break;
   }
+
 
   tiler.queueImage(mainX, mainY, std::move(mainImage));
   // Tile images horizontally first and then vertically
   if(tileType == 0)
   {
-    tiler.tileImages(outputWidth, outputHeight-mainImageHeight, 0, (mainY == 0 ? mainImageHeight : 0), minDraw, 0);
-    tiler.tileImages(outputWidth-mainImageWidth, mainImageHeight, (mainX == 0 ? mainImageWidth : 0), (mainY == 0 ? 0 : outputHeight - mainImageHeight), minDraw, 1);
+    tiler.tileImages(FLAGS_outputWidth, FLAGS_outputHeight-mainImageHeight, 0, (mainY == 0 ? mainImageHeight : 0), FLAGS_minDraw, 0);
+    tiler.tileImages(FLAGS_outputWidth-mainImageWidth, mainImageHeight, (mainX == 0 ? mainImageWidth : 0), (mainY == 0 ? 0 : FLAGS_outputHeight - mainImageHeight), FLAGS_minDraw, 1);
   }
   // Tile images vertically first and then horizontally
   else
   {
-    tiler.tileImages(outputWidth-mainImageWidth, outputHeight, (mainX == 0 ? mainImageWidth : 0), 0, minDraw, 1);
-    tiler.tileImages(mainImageWidth, outputHeight-mainImageHeight, mainX,  (mainY == 0 ? mainImageHeight : 0), minDraw, 0);
+    tiler.tileImages(FLAGS_outputWidth-mainImageWidth, FLAGS_outputHeight, (mainX == 0 ? mainImageWidth : 0), 0, FLAGS_minDraw, 1);
+    tiler.tileImages(mainImageWidth, FLAGS_outputHeight-mainImageHeight, mainX,  (mainY == 0 ? mainImageHeight : 0), FLAGS_minDraw, 0);
   }
   tiler.draw();
-  tiler.canvas.write(filename);
+  tiler.canvas.write(FLAGS_outputFilename);
 
   return 0;
 }
