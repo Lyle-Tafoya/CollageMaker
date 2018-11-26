e0d3d4aebef6b8f3bc25cb6a3c3c275d0e396377
Lyle-Tafoya <lyle.tafoya@gmail.com>
2018-11-26 09:10:36 -0800

    Updated command line arguments

--- README.md
+++ README.md
@@ -7,22 +7,20 @@ Here is a downscaled sample output file
 
 #### Command Line Arguments:
 ```
-Argument      | Effect
---------------|----------------------------------------------------------------
---color,   -c | Background color for the canvas. Defaults to "#000".
---corner,  -r | Corner to place the first image. Defaults to 0(top-left going clockwise)
---file,    -f | File name. Format is set based on extension. Defaults to "collage.png".
---height,  -h | Height of the canvas in pixels. Defaults to 1080.
---path,    -p | Specify path to a file containing a list of image path names
---minDraw, -d | Minimum number of pixels needed to tile images in. Defaults to 150.
---size,    -s | Set the size of the first image as a percentage relative to the canvas.
---type,    -t | Choose whether to tile "horizontal" or "vertical" first.
---width,   -w | Set the width of the final collage in pixels. Defaults to 1920.
+Argument     | Effect
+-------------|----------------------------------------------------------------
+-bgcolor     | Background color for the canvas. Defaults to "#000".
+-corner      | Corner to place the first image. Defaults to 0(top-left going clockwise)
+-height      | Height of the canvas in pixels. Defaults to 1080.
+-input       | Specify path to a file containing a list of image path names
+-minDraw     | Minimum number of pixels needed to tile images in. Defaults to 150.
+-output      | File name. Format is set based on extension. Defaults to "collage.png".
+-size        | Set the size of the first image as a percentage relative to the canvas.
+-tileType    | Choose whether to tile "horizontal" or "vertical" first.
+-width       | Set the width of the final collage in pixels. Defaults to 1920.
 ```
 
-**If --path is not set, CollageMaker will look for a file "./images.txt".** This is simply a plain text file containing one absolute path to an image per line. To generate this file, run the following command in the root of your image folder _OSX/Linux_
+**If --input is not set, CollageMaker will look for a file "./images.txt".** This is simply a plain text file containing one absolute path to an image per line. To generate this file, run the following command in the root of your image folder
 ````
 ls -d -1 $PWD/**/*.[jpg,png,bmp]* > images.txt
 ````
-
-If you are using Windows, you will have to find another way to generate this file.
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,16 @@
 #include <cstdlib>
 #include <ctime>
-#include <cstring>
-#include <iostream>
-#include <memory>
-#include <string>
-#include <vector>
 
 #include <gflags/gflags.h>
 #include <Magick++.h>
 
 #include "tiler.hpp"
 
-DEFINE_int32(outputWidth, 1920, "Width of the output image");
-DEFINE_int32(outputHeight, 1080, "Height of the output image");
-DEFINE_string(canvasColor, "black", "Color of the canvas background");
-DEFINE_string(outputFilename, "collage.png", "Ouput filename for the collage image");
-DEFINE_string(inputFilenames, "images.txt", "image filepath list");
+DEFINE_int32(width, 1920, "Width of the output image");
+DEFINE_int32(height, 1080, "Height of the output image");
+DEFINE_string(bgcolor, "black", "Color of the canvas background");
+DEFINE_string(output, "collage.png", "Ouput filename for the collage image");
+DEFINE_string(input, "images.txt", "image filepath list");
 DEFINE_int32(minDraw, 150, "Minimum amount of pixels to tile images in");
 DEFINE_int32(corner, 0, "Specify which corner to place first image");
 DEFINE_int32(size, 65, "Size of the first image as a percentage relative to the canvas");
@@ -27,16 +22,15 @@ int main(int argc, char *argv[])
 
   gflags::SetUsageMessage("Generate image collages");
   gflags::ParseCommandLineFlags(&argc, &argv, true);
-  bool tileType = (FLAGS_tileType == "vertical" || FLAGS_tileType == "v") ? 1 : 0;
+  bool tileType = (FLAGS_tileType == "horizontal" || FLAGS_tileType == "h") ? 0 : 1;
 
-  CollageMaker::Tiler tiler(FLAGS_outputWidth, FLAGS_outputHeight, FLAGS_inputFilenames, FLAGS_canvasColor);
+  CollageMaker::Tiler tiler(FLAGS_width, FLAGS_height, FLAGS_input, FLAGS_bgcolor);
 
   size_t imageNum = CollageMaker::randNum(0, tiler.imageFilePaths.size()-1);
   Magick::Image mainImage(tiler.popImagePath(imageNum));
-  // Downscale the image to the specified size but never upscale
-  if(static_cast<float>(FLAGS_outputWidth)/mainImage.columns() < static_cast<float>(FLAGS_outputHeight)/mainImage.rows())
+  if(static_cast<float>(FLAGS_width)/mainImage.columns() < static_cast<float>(FLAGS_height)/mainImage.rows())
   {
-    size_t imageWidth = static_cast<float>(FLAGS_size) / 100.f * FLAGS_outputWidth;
+    size_t imageWidth = static_cast<float>(FLAGS_size) / 100.f * FLAGS_width;
     if(mainImage.columns() > imageWidth)
     {
       mainImage.resize(Magick::Geometry(std::to_string(imageWidth)));
@@ -44,7 +38,7 @@ int main(int argc, char *argv[])
   }
   else
   {
-    size_t imageHeight = static_cast<float>(FLAGS_size) / 100.f * FLAGS_outputHeight;
+    size_t imageHeight = static_cast<float>(FLAGS_size) / 100.f * FLAGS_height;
     if(mainImage.rows() > imageHeight)
     {
       mainImage.resize(Magick::Geometry("x" + std::to_string(imageHeight)));
@@ -54,7 +48,7 @@ int main(int argc, char *argv[])
   const size_t mainImageHeight = mainImage.rows();
 
   size_t mainX = 0, mainY = 0;
-  switch(FLAGS_corner)
+  switch(FLAGS_corner % 4)
   {
     // Top Left
     case 0:
@@ -63,18 +57,18 @@ int main(int argc, char *argv[])
       break;
     // Top Right
     case 1:
-      mainX = FLAGS_outputWidth - mainImageWidth;
+      mainX = FLAGS_width - mainImageWidth;
       mainY = 0;
       break;
     // Bottom Right
     case 2:
-      mainX = FLAGS_outputWidth - mainImageWidth;
-      mainY = FLAGS_outputHeight - mainImageHeight;
+      mainX = FLAGS_width - mainImageWidth;
+      mainY = FLAGS_height - mainImageHeight;
       break;
     // Bottom Left
     case 3:
       mainX = 0;
-      mainY = FLAGS_outputHeight - mainImageHeight;
+      mainY = FLAGS_height - mainImageHeight;
       break;
   }
 
@@ -83,17 +77,17 @@ int main(int argc, char *argv[])
   // Tile images horizontally first and then vertically
   if(tileType == 0)
   {
-    tiler.tileImages(FLAGS_outputWidth, FLAGS_outputHeight-mainImageHeight, 0, (mainY == 0 ? mainImageHeight : 0), FLAGS_minDraw, 0);
-    tiler.tileImages(FLAGS_outputWidth-mainImageWidth, mainImageHeight, (mainX == 0 ? mainImageWidth : 0), (mainY == 0 ? 0 : FLAGS_outputHeight - mainImageHeight), FLAGS_minDraw, 1);
+    tiler.tileImages(FLAGS_width, FLAGS_height-mainImageHeight, 0, (mainY == 0 ? mainImageHeight : 0), FLAGS_minDraw, 0);
+    tiler.tileImages(FLAGS_width-mainImageWidth, mainImageHeight, (mainX == 0 ? mainImageWidth : 0), (mainY == 0 ? 0 : FLAGS_height - mainImageHeight), FLAGS_minDraw, 1);
   }
   // Tile images vertically first and then horizontally
   else
   {
-    tiler.tileImages(FLAGS_outputWidth-mainImageWidth, FLAGS_outputHeight, (mainX == 0 ? mainImageWidth : 0), 0, FLAGS_minDraw, 1);
-    tiler.tileImages(mainImageWidth, FLAGS_outputHeight-mainImageHeight, mainX,  (mainY == 0 ? mainImageHeight : 0), FLAGS_minDraw, 0);
+    tiler.tileImages(FLAGS_width-mainImageWidth, FLAGS_height, (mainX == 0 ? mainImageWidth : 0), 0, FLAGS_minDraw, 1);
+    tiler.tileImages(mainImageWidth, FLAGS_height-mainImageHeight, mainX,  (mainY == 0 ? mainImageHeight : 0), FLAGS_minDraw, 0);
   }
   tiler.draw();
-  tiler.canvas.write(FLAGS_outputFilename);
+  tiler.canvas.write(FLAGS_output);
 
   return 0;
 }
